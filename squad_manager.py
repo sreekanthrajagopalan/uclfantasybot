6f52b649fb328bfdb9347c26a3c0f1b867de1542
Sreekanth Rajagopalan <sreekanr@gmail.com>
2021-11-19 23:05:45 +0000

    add basic sets of constraints and objective

--- squad_manager.py
+++ squad_manager.py
@@ -55,6 +55,34 @@ def get_players_info(gameday_id: int) -> Response:
     return req
 
 
+# define basic sets of constraints
+def define_basic_constraints(model, matchday: int, stage: str) -> None:
+    """Define basic sets of constraints in the MIP"""
+
+    # required number of players by skills
+    def rule_ReqdPlayersBySkills(m, skill):
+        return sum(m.ySelectPlayer[p] for p in model.sPlayersWithSkills[skill]) \
+            <= m.pReqdPlayersBySkills[skill]
+    model.cReqdPlayersBySkills = pyo.Constraint(model.sSkills,
+        rule=rule_ReqdPlayersBySkills)
+
+    # player limit per club
+    def rule_LimPlayersPerClub(m, club):
+        return sum(m.ySelectPlayer[p] for p in model.sPlayersInClubs[club]) \
+            <= m.pLimPlayersPerClub[stage]
+    model.cLimitPlayersPerClub = pyo.Constraint(model.sClubs,
+        rule=rule_LimPlayersPerClub)
+
+    # cannot exceed budget
+    def rule_Budget(m):
+        return sum(m.pPlayerPrices[p] * m.ySelectPlayer[p] \
+            for p in model.sPlayers) \
+            <= m.pBudget[matchday]
+    model.cBudget = pyo.Constraint(rule=rule_Budget)
+
+    #TODO: transfer rules
+
+
 # function to select the best squad for a given matchday
 def select_matchday_squad(df_player_info: pd.DataFrame, matchday: int,
     current_squad: list = None) -> list:
@@ -67,17 +95,39 @@ def select_matchday_squad(df_player_info: pd.DataFrame, matchday: int,
     model.sPlayers = pyo.Set(initialize=list(df_player_info['id']),
         doc='Set of players')
 
+    # subset of active players
+    model.sActivePlayers = pyo.Set(
+        initialize=list(df_player_info[df_player_info['isActive'] == 1]['id']),
+        doc='Set of active players')
+    model.sInactivePlayers = model.sPlayers - model.sActivePlayers
+
+    # subset of players unavailable for selection
+    model.sAvailablePlayers = pyo.Set(
+        initialize=list(df_player_info[df_player_info['trained'].str.match(
+            'In contention to start next game')]['id']),
+        doc='Set of players available for selection')
+    model.sUnavailablePlayers = model.sPlayers - model.sAvailablePlayers
+
     # set of player skills
     model.sSkills = pyo.Set(initialize=[1, 2, 3, 4], doc='Set of player skills')
 
+    # set of players by skills
+    def sPlayersWithSkills_init(m, skill):
+        for player in list(df_player_info[df_player_info['skill'] == skill]['id']):
+            yield player
+    model.sPlayersWithSkills = pyo.Set(model.sSkills, initialize=sPlayersWithSkills_init,
+        doc='Set of players with a given skill')
+
     # set of clubs
-    model.sClubs = pyo.Set(initialize=list(df_player_info['cCode'].unique()))
+    model.sClubs = pyo.Set(initialize=list(df_player_info['cCode'].unique()),
+        doc='Set of clubs')
 
     # set of players in clubs
     def sPlayersInClubs_init(m, club):
         for player in list(df_player_info[df_player_info['cCode'] == club]['id']):
             yield player
-    model.sPlayersInClubs = pyo.Set(model.sClubs, initialize=sPlayersInClubs_init)
+    model.sPlayersInClubs = pyo.Set(model.sClubs, initialize=sPlayersInClubs_init,
+        doc='Set of players in a given club')
 
     # set of matchdays
     num_matchdays = 13
@@ -95,7 +145,8 @@ def select_matchday_squad(df_player_info: pd.DataFrame, matchday: int,
     def sMatchdaysInStages_init(m, stage):
         for matchday in matchdays_in_stages[stage]:
             yield matchday
-    model.sMatchdaysInStages = pyo.Set(model.sStages, initialize=sMatchdaysInStages_init)
+    model.sMatchdaysInStages = pyo.Set(model.sStages, initialize=sMatchdaysInStages_init,
+        doc='Set of matchdays in a given stage')
 
     # param: required number of players by skills in a squad
     model.pReqdPlayersBySkills = pyo.Param(model.sSkills, initialize={1:2, 2:5, 3:5, 4:3},
@@ -111,17 +162,73 @@ def select_matchday_squad(df_player_info: pd.DataFrame, matchday: int,
         5:2, 6:2, 7:15, 8:3, 9:5, 10:3, 11:5, 12:3, 13:5},
         doc="Free transfers before matchdays")
 
-    #TODO: declare variables
+    # param: budget
+    def pBudget_init(m, matchday):
+        if matchday > 6:
+            return 105
+        return 100
+    model.pBudget = pyo.Param(model.sMatchdays, initialize=pBudget_init,
+        doc='Budget for the matchday')
 
-    #TODO: define constraints
+    # param: player prices
+    def pPlayerPrices_init(m, player):
+        return df_player_info[df_player_info['id'] == player]['value'].iloc[0]
+    model.pPlayerPrices = pyo.Param(model.sPlayers, initialize=pPlayerPrices_init,
+        doc='Player prices')
 
-    #TODO: define objective
+    # param: player average points
+    def pPlayerAvgPoints_init(m, player):
+        return df_player_info[df_player_info['id'] == player]['avgPlayerPts'].iloc[0]
+    model.pPlayerAvgPoints = pyo.Param(model.sPlayers, initialize=pPlayerAvgPoints_init,
+        doc='Player average points')
 
-    #TODO: solve MIP to get best squad
+    # param: player last game day points
+    def pPlayerLastGdPoints_init(m, player):
+        return df_player_info[df_player_info['id'] == player]['lastGdPoints'].iloc[0]
+    model.pPlayerLastGdPoints = pyo.Param(model.sPlayers, initialize=pPlayerLastGdPoints_init,
+        doc='Player last game day points')
 
-    #TODO: return best squad
+    # var: binary indicating if a player is selected for matchday squad
+    model.ySelectPlayer = pyo.Var(model.sPlayers, domain=pyo.Binary)
 
-    return []
+    # utils
+    def find_key(d, v):
+        for k, vs in d.items():
+            if v in vs:
+                return k
+    stage = find_key(matchdays_in_stages, matchday)
+
+    # define basic sets of constraints
+    define_basic_constraints(model, matchday, stage)
+
+    # constraint: exclude inactive players
+    for p in model.sInactivePlayers:
+        model.ySelectPlayer[p].fix(0)
+
+    #TODO: constraint: exclude players not available for selection
+    # Why is 'trained' field '' between matchdays?
+    #for p in model.sUnavailablePlayers:
+    #    model.ySelectPlayer[p].fix(0)
+
+    # define objective
+    form_weight = 0.5
+    def objMaxAvgPointsFormWeighted(m):
+        return sum(((1-form_weight) * m.pPlayerAvgPoints[p] + form_weight * m.pPlayerLastGdPoints[p])\
+            * m.ySelectPlayer[p] for p in model.sPlayers)
+    model.objMaxAvgPointsFormWeighted = pyo.Objective(rule=objMaxAvgPointsFormWeighted,
+        sense=pyo.maximize)
+
+    # solve MIP to get best squad
+    opt = pyo.SolverFactory('cbc')
+    opt.solve(model, tee=True)
+
+    # return best squad
+    opt_squad = []
+    for p in model.sPlayers:
+        if pyo.value(model.ySelectPlayer[p]) >= 0.9999:
+            opt_squad.append(df_player_info[df_player_info['id'] == p]['pDName'].iloc[0])
+
+    return opt_squad
 
 
 # main function
@@ -154,7 +261,8 @@ def main():
         #TODO: get current squad
 
         #TODO: select best squad
-        select_matchday_squad(df_player_info, 5)
+        opt_squad = select_matchday_squad(df_player_info, 5)
+        print(opt_squad)
 
         # logout of the session
         res = session_logout()
